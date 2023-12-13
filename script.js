12310a6f45602a196aa356a8be180e41b5c54679
Lance Tse <steam.bao666@gmail.com>
2023-12-13 12:22:44 -0800

    'wet code checkerboard, compress styles next'

--- script.js
+++ script.js
@@ -1,34 +1,62 @@
+/*
 //create container for tiles, checkerboard
-let tileContainerStyles = {
-    display: 'flex',
-    flexWrap: 'wrap',
-    width: '800px',
-    height: '800px'
+let checkerboardStyles = {
+    display = 'flex';
+    flexWrap = 'wrap';
+    height = '800px';
+    width = '800px';
+    backgroundColor = 'white';
 }
 
 //create tile object
-let tileStyles = {
+let tileStylesRed = {
     width: '12.5%',
-    height: '12.5%'
+    height: '12.5%',
+    backgroundColor: 'red'
 }
 
-let checkerboard = document.createElement('checkerboard');
+let tileStylesBlack = {
+    width: '12.5%',
+    height: '12.5%',
+    backgroundColor: 'black'
+}
+*/
+
+const body = document.body;
+let checkerboard = document.createElement('div');
+    checkerboard.id = 'checkerboard';
+    checkerboard.style.display = 'flex';
+    checkerboard.style.flexWrap = 'wrap';
+    checkerboard.style.height = '800px';
+    checkerboard.style.width = '800px';
+    checkerboard.style.backgroundColor = 'white';
+document.body.appendChild(checkerboard);
+
 //create the checkerboard board 8x8
 //create container to store tiles (row of 8 tiles)
 for (let row = 0; row < 8; row++) {
-    let tileContainer = document.createElement('tileContainer');
+    let tileRow = document.createElement('div');
+        tileRow.style.display = 'flex';
+        tileRow.style.height = '12.5%';
+        tileRow.style.width = '100%';
     //create each tile at row
     for (let tileAtRow = 0; tileAtRow < 8; tileAtRow++) {
-        let tile = document.createElement('tile');
+        //let tile = document.createElement('tile');
         if (row % 2 === tileAtRow % 2) {
-            tile.className = 'red';
+            let tile = document.createElement('div');
+                tile.style.height = '100%';
+                tile.style.width = '12.5%';
+                tile.style.backgroundColor = 'red';
+            tileRow.appendChild(tile);
         } else {
-            tile.className = 'black';
+            tile = document.createElement('div');
+                tile.style.height = '100%';
+                tile.style.width = '12.5%';
+                tile.style.backgroundColor = 'black';
+            tileRow.appendChild(tile);
         }
-        tileContainer.appendChild(tile);
+        checkerboard.appendChild(tileRow);
 
     }
-    //append each row when columns are completed in inner loop
-    checkerboard.appendChild(tileContainer);
 }
 document.body.appendChild(checkerboard);
